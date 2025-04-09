fadda3850655a3bee4f05315020cc5c058ff27e3
JRodriAlarc <jona10131313@gmail.com>
2025-04-09 13:12:20 -0600

    Feat(follow-us) Redes Sociales

---
+++ src/app/components/follow-us/follow-us.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-follow-us',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './follow-us.component.html',
+  styleUrl: './follow-us.component.css'
+})
+export class FollowUsComponent {
+
+}
