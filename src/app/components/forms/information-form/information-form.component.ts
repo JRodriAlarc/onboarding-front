e4cb2dfa499b7ea0a3a167e9825e0eadf0a00e3f
JRodriAlarc <jona10131313@gmail.com>
2025-04-10 11:17:16 -0600

    Feat(information-form) Formulario para registrar y actualizar los datos del usuario

---
+++ src/app/components/forms/information-form/information-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './information-form.component.html',
+  styleUrl: './information-form.component.css'
+})
+export class InformationFormComponent {
+
+}
